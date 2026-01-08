95ade5b78b24909b0b8ad91d695149ae0b690f22
Barang <barang@gmail.com>
2026-01-08 11:06:24 +0700

    admincss

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <h1><center>Admin Page</center></h1>
+        <h1>Admin Page</h1>
     </div>
 </template>
 <style>
     h1 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         color: rgb(67, 20, 177);
     }
 </style>
