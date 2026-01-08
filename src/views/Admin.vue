07c3c9f6442fc3d05cef4d128b1fbb945701cdc0
Barang <barang@gmail.com>
2026-01-08 11:17:00 +0700

    new

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,17 +1,26 @@
 <template>
-    <div>
-        <h1>Admin Page</h1>
-    </div>
-    <router-link to="/home">
-      <button>Click to home</button>
+  <div class="admin">
+    <h1>Admin Page</h1>
+
+    <router-link to="/">
+      <button>Go to Home</button>
     </router-link>
+  </div>
 </template>
-<style>
-    h1 {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: rgb(67, 20, 177);
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    }
+
+<style scoped>
+.admin {
+  text-align: center;
+}
+
+h1 {
+  color: rgb(67, 20, 177);
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
 </style>
